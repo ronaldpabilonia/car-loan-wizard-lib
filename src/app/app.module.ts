d2f0e9bcedaf873a7646afe631f6100cb121b4eb
DESKTOP-CC7PDB0\Ronald <Ronald.Parker@firstmac.com.au>
2021-09-03 09:50:52 +0800

    added first name and last name input components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstNameComponent } from './first-name/first-name.component';
+import { LastNameComponent } from './last-name/last-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstNameComponent,
+    LastNameComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  exports: [
+    FirstNameComponent,
+    LastNameComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/first-name/first-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-name',
+  templateUrl: './first-name.component.html',
+  styleUrls: ['./first-name.component.scss']
+})
+export class FirstNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
